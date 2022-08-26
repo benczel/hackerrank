3578fe361102d704079e7cb694026effcea74d1b
Laszlo Bencze <bencze.laszlo22@gmail.com>
2022-08-26 16:16:42 +0200

    changed lpad

--- sql/sql_advanced/draw_the_triangle_1/solution.sql
+++ sql/sql_advanced/draw_the_triangle_1/solution.sql
@@ -2,5 +2,6 @@ with data(r) as (
     select 1 r from dual
     union all 
     select r+1 from data where r < 20
-)select lpad('* ',r, ' *') from data
+)select lpad('*',2*21 - 2*level,' *')
+from data
 order by r desc;
