197c21e1861b236541ba9362a9f60261aa133de3
Laszlo Bencze <bencze.laszlo22@gmail.com>
2022-09-20 09:41:16 +0200

    making more functional

--- functional_programming/introduction/filter_positions_in_a_list/src/main/scala/hu/hackerrank/filter/positions/Solution2.scala
+++ functional_programming/introduction/filter_positions_in_a_list/src/main/scala/hu/hackerrank/filter/positions/Solution2.scala
@@ -3,7 +3,7 @@ package hu.hackerrank.filter.positions
 object Solution2 {
 
   def f(arr:List[Int]):List[Int] = {
-   arr.zipWithIndex filter(i => i._2 % 2 != 0) map (i => i._1)
+   arr.zipWithIndex filter(i => i._2 % 2 != 0) map (_._1)
   }
 
   def main(args:Array[String]):Unit = {
